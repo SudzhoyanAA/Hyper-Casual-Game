92c74702bc77254d7ef17c1d026b0edd3d212dcb
Sloykina <sloyy1@gmail.com>
2021-12-08 11:22:54 +0300

    Исправленное взаимодействие стрелки с цветами

--- Game/Assets/Scripts/Arrow.cs
+++ Game/Assets/Scripts/Arrow.cs
@@ -4,33 +4,6 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    public float force;
-    public GameObject arrowPrefab;
-    GameObject arrow;
-
-    public float repeat_time; 
-    private float curr_time;
-    
-    void Start()
-    {
-        arrow = Instantiate(arrowPrefab, transform);
-        curr_time = repeat_time * 1000f;
-    }
-    void Update()
-    {
-        curr_time -= Time.deltaTime;
-        if (curr_time <= 0)
-        {
-            arrow = Instantiate(arrowPrefab, transform);
-            curr_time = repeat_time * 1000f;
-        }
-    }   
-    void FixedUpdate()
-    {
-        arrow.transform.parent = null;
-        arrow.GetComponent<Rigidbody2D>().AddForce(Vector2.up * force, ForceMode2D.Impulse);
-    }
-
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Green")
