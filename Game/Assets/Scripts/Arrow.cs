f8b6752e632f3296e038c16bb6ba01191f28072c
SudzhoyanAA <86315030+SudzhoyanAA@users.noreply.github.com>
2021-12-14 20:56:43 +0300

    Menu

--- Game/Assets/Scripts/Arrow.cs
+++ Game/Assets/Scripts/Arrow.cs
@@ -4,23 +4,56 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    public float speed;
-
-    void FixedUpdate()
-    {
-        transform.position += Vector3.up * speed * Time.deltaTime;
-    }
-
+    //Взаимодействие стрелок с цветами
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Green")
+        if (this.tag == "Green arrow")
         {
-            Destroy(GameObject.FindGameObjectWithTag("Circle"));
-            Destroy(GameObject.FindGameObjectWithTag("Green arrow"));
+            if (col.gameObject.tag == "Green")
+            {
+                Destroy(col.transform.parent.gameObject);
+                Destroy(GameObject.FindGameObjectWithTag("Green arrow"));
+            }
+            else
+            {
+                Destroy(GameObject.FindGameObjectWithTag("Green arrow"));
+            }
         }
-        else
+        else if (this.tag == "Red arrow")
         {
-            Destroy(GameObject.FindGameObjectWithTag("Green arrow"));
+            if (col.gameObject.tag == "Red")
+            {
+                Destroy(col.transform.parent.gameObject);
+                Destroy(GameObject.FindGameObjectWithTag("Red arrow"));
+            }
+            else
+            {
+                Destroy(GameObject.FindGameObjectWithTag("Red arrow"));
+            }
+        }
+        else if (this.tag == "Purple arrow")
+        {
+            if (col.gameObject.tag == "Purple")
+            {
+                Destroy(col.transform.parent.gameObject);
+                Destroy(GameObject.FindGameObjectWithTag("Purple arrow"));
+            }
+            else
+            {
+                Destroy(GameObject.FindGameObjectWithTag("Purple arrow"));
+            }
+        }
+        else if (this.tag == "Orange arrow")
+        {
+            if (col.gameObject.tag == "Orange")
+            {
+                Destroy(col.transform.parent.gameObject);
+                Destroy(GameObject.FindGameObjectWithTag("Orange arrow"));
+            }
+            else
+            {
+                Destroy(GameObject.FindGameObjectWithTag("Orange arrow"));
+            }
         }
     }
 }
---
+++ Game/Assets/Scripts/BRArrowSpawner.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BRArrowSpawner : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    GameObject arrow;
+
+    private float timer = 0;
+    public float start_delay;
+    public float timing;
+
+    public float speed;
+
+    Material[] colors = new Material[4];
+    public Material greenMaterialRef;
+    public Material redMaterialRef;
+    public Material purpleMaterialRef;
+    public Material orangeMaterialRef;
+
+    void Start()
+    {
+        colors[0] = greenMaterialRef;
+        colors[1] = redMaterialRef;
+        colors[2] = purpleMaterialRef;
+        colors[3] = orangeMaterialRef;
+
+        //Первая генерация стрелки с определенной задержкой
+        Invoke("ArrowInstantiate", start_delay);
+    }
+
+    void FixedUpdate()
+    {
+        //Последующие генерации срелки с определенным интервалом 
+        timer += Time.deltaTime;
+        if (timer >= timing)
+        {
+            Invoke("ArrowInstantiate", start_delay);
+            timer = 0;
+        }
+        //Движение стрелки
+        if (arrow != null)
+        {
+            arrow.GetComponent<Rigidbody2D>().velocity = new Vector3(Screen.width / -2, Screen.height / 2, 0) * speed;
+        }
+    }
+
+    void ArrowInstantiate()
+    {
+        //Рассчет позиции угла экрана
+        Bounds bounds = GetComponent<SpriteRenderer>().bounds;
+        Vector3 bottomLeftPosition = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0, Camera.main.nearClipPlane));
+        bottomLeftPosition += new Vector3(bounds.size.x / 2, -bounds.size.y / 2, 0);
+        //Генерация стрелки в этой позиции
+        arrow = Instantiate(arrowPrefab, bottomLeftPosition, Quaternion.Euler(0, 0, -35));
+
+        //Рандомное присваивание цвета стрелке
+        arrow.GetComponent<Renderer>().material = colors[Random.Range(0, colors.Length)];
+        //Присваивание соответствующего тега стрелке
+        if (arrow.GetComponent<Renderer>().sharedMaterial == redMaterialRef)
+        {
+            arrow.tag = "Red arrow";
+        }
+        else if (arrow.GetComponent<Renderer>().sharedMaterial == purpleMaterialRef)
+        {
+            arrow.tag = "Purple arrow";
+        }
+        else if (arrow.GetComponent<Renderer>().sharedMaterial == orangeMaterialRef)
+        {
+            arrow.tag = "Orange arrow";
+        }
+    }
+}
+
